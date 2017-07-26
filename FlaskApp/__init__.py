1ec00b941f638de2a65f739a1ddb1923161ff91c
meagdoh <meagdoherty@gmail.com>
2017-07-26 13:33:47 -0400

    Add index.html template

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hi there, how ya doin Chris?"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
