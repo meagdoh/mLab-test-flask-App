cb84641b6161c4f3be171926cf21f96109559bc9
meagdoh <meagdoherty@gmail.com>
2017-08-02 11:39:57 -0400

    add form

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     return render_template("index.html")
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        region = request.form['region']
+
+    return render_template('region.html', region=region)
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
